fa0fd0a08ed303ee17157a0c12a23d196a1ab5ec
Arthur Lopes <arthurlopr12@gmail.com>
2023-07-04 21:10:50 -0300

    feat: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { GlobalStyles } from "./styles/global";
 
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyles/> 
     </div>
   );
 }
