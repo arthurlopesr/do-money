6eb18fbbea81f9043bb1161ed53e4d3fac68bea5
Arthur Lopes <arthurlopr12@gmail.com>
2023-07-04 21:37:13 -0300

    feat: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Header } from "./components/header/Header";
 import { GlobalStyles } from "./styles/global";
 
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <GlobalStyles/> 
-    </div>
+    <>
+      <Header />
+      <GlobalStyles />
+    </>
   );
 }
 
