d7ca6f6031013e5b4708aaa426e107bdfc99ab2b
Arthur Lopes <arthurlopr12@gmail.com>
2023-07-07 11:49:22 -0300

    feat: add information in db with MirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs';
+import { Model, createServer } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+      transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de websit',
+          type: 'deposit',
+          category: 'Dev',
+          amouth: 6000,
+          createdAt: new Date('2023-02-05 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amouth: 750,
+          createdAt: new Date('2023-02-07 11:00:00'),
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+
     })
   }
 })
